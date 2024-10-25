a8a04ac6eb66474d965abb87effe340d75f33229
eldieguitoamaya <diego.amaya@estudiants.urv.cat>
2024-10-25 09:00:33 +0200

    commit 2 examen

--- code.js
+++ code.js
@@ -1,14 +1,21 @@
-BASE_URL="https://pokeapi.co/api/v2/"
-
-function getKantoPokemon(){
-    const url = `${BASE_URL}pokemon/?limit=151`;
-    const ulKanto = document.getElementById("ulKanto");
-    for( let i = 1; i <= 151; i++){
-        response = fetch(`${BASE_URL}pokemon/${i}`)
-        data = response.json();
-        const li = document.createElement("li");
-        li.innerHTML = data.results[i].name;
+const BASE_URL="https://pokeapi.co/api/v2/"
+window.onload = async function(){
+    const pokemons = await getKantoPokemon();
+    const ulKanto = document.getElementById("kanto-list");
+    for (const pokemon of pokemons){
+        const listElement = document.createElement("li");
+        listElement.textContent = pokemon.name;
         ulKanto.appendChild(li);
+    }
+
+}
+async function getKantoPokemon(){
+   
+    for( let i = 1; i <= 151; i++){
+        const response = await fetch (`${BASE_URL}pokemon/?limit=151`);
+        const jsonresponse = await response.json();
+        const pokemonArray = jsonresponse.results;
+        return pokemonArray;
     }
     
 }
